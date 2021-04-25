23fe4cedb646cf768ae091e4169c4cf0569a0c10
AbdAlrahmanDaas963 <81186441+AbdAlrahmanDaas963@users.noreply.github.com>
2021-04-26 02:57:01 +0300

    title2

---
+++ css/main.js
@@ -0,0 +1,36 @@
+function sti1()
+{
+    document.querySelector(".sti").src=("img/sti1.jpg");
+}
+function sti2()
+{
+    document.querySelector(".sti").src=("img/sti2.jpg");
+}
+function sti3()
+{
+    document.querySelector(".sti").src=("img/sti3.jpg");
+}
+function sti4()
+{
+    document.querySelector(".sti").src=("img/sti4.jpg");
+}
+function sti5()
+{
+    document.querySelector(".sti").src=("img/sti5.jpg");
+}
+function sti6()
+{
+    document.querySelector(".sti").src=("img/sti6.jpg");
+}
+function sti7()
+{
+    document.querySelector(".sti").src=("img/sti7.jpg");
+}
+function sti8()
+{
+    document.querySelector(".sti").src=("img/sti8.jpg");
+}
+function sti9()
+{
+    document.querySelector(".sti").src=("img/sti9.jpg");
+}
